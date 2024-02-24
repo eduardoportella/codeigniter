5fae43072d36af162fd94bd17451d7ca19e18213
Eduardo Portella <eportella70@gmail.com>
2024-02-24 19:32:55 -0300

    aula 2: inserindo template

---
+++ application/views/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>PHPJabbers.com | Free Car Rental Website Template</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" />
+		<link rel="stylesheet" href="<?php echo base_url('assets/css/main.css') ?> " />
+		<noscript><link rel="stylesheet" href="<?php echo base_url('assets/css/noscript.css') ?>" /></noscript>
+	</head>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+							<!-- Logo -->
+								<a href="index.php" class="logo">
+									<span class="fa fa-car"></span> <span class="title">CAR RENTAL WEBSITE</span>
+								</a>
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+						</div>
+					</header>
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="index.php" class="active">Home</a></li>
+
+							<li><a href="offers.php">Offers</a></li>
+
+							<li><a href="fleet.php">Fleet</a></li>
+
+							<li>
+								<a href="#" class="dropdown-toggle">About</a>
+
+								<ul>
+									<li><a href="about.php">About Us</a></li>
+									<li><a href="team.php">Team</a></li>
+									<li><a href="blog.php">Blog</a></li>
+									<li><a href="testimonials.php">Testimonials</a></li>
+									<li><a href="faq.php">FAQ</a></li>
+									<li><a href="terms.php">Terms</a></li>
+								</ul>
+							</li>
+							<li><a href="contact.php">Contact Us</a></li>
+						</ul>
+					</nav>
